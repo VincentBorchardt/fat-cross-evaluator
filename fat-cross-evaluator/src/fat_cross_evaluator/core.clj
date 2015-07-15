867d20ba73cc4d92c46ae32999d55aeb455c502a
VincentBorchardt <vborchardt123@gmail.com>
2015-07-14 23:05:33 -0500

    Initial commit, start of things

---
+++ fat-cross-evaluator/src/fat_cross_evaluator/core.clj
@@ -0,0 +1,7 @@
+(ns fat-cross-evaluator.core)
+
+(defn fat-cross-evaluator [player-num cap-dists tile-array strat-weights]
+  1 )
+
+(defn total-weights [weight-map]
+  (reduce + (map val weight-map)))
---
+++ fat-cross-evaluator/test/fat_cross_evaluator/core_test.clj
@@ -0,0 +1,36 @@
+(ns fat-cross-evaluator.core-test
+  (:use clojure.test
+        fat-cross-evaluator.core))
+
+;(deftest a-test
+;  (testing "FIXME, I fail."
+;    (is (= 0 1))))
+
+; parameters received:
+; player number
+; hash of player number -> distances (including own)
+; array of 21 tiles (below)
+; hash map of strategic weights
+
+; tiles: hash map of:
+; x = integer, relative to bfc-center
+; y = integer, relative to bfc-center
+; terrain = grass/plains/tundra/desert/ice
+; bonus = corn/gems/etc
+; height = 0/1/2/3 for peak/hill/flat/water
+; feature = jungle/forest/oasis
+; auto-routed = true or false (e.g. if a city is planted here, will it automatically connect to an existing city that you own?)
+; adjacent to river = true or false
+; shared_with_allied_city = true or false
+; shared_with_enemy_city = true or false
+
+(def simple-weights
+  {:city-on-hill 0.2
+   :has-copper 0.9
+   :has-iron 0.0
+   :has-ancient-luxury 0.6
+   :has-classical-luxury 0.0})
+
+(deftest total-weights-test
+  (testing "total-weights works"
+           (is (= (total-weights simple-weights) 1.7))))
